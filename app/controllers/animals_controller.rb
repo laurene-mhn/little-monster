95df0b1ca5738dda6e7286cd199f7f86a487f203
Gregoire Lemesle <gregoire.lemesle@gmail.com>
2020-11-17 16:59:07 +0100

    Add rules for pundit in the animals controller and in the app and animal policy file

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -11,10 +11,12 @@ class AnimalsController < ApplicationController
 
   def new
     @animal = Animal.new
+    authorize @animal
   end
 
   def create
     @animal = Animal.new(animal_params)
+    @animal.user = current_user
     if @animal.save
       redirect_to animal_path(@animal), notice: "Animal was successfully created"
     else
---
+++ app/policies/animal_policy.rb
@@ -0,0 +1,23 @@
+class AnimalPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    false
+  end
+
+  def destroy?
+    false
+  end
+end
