2344a65c0201d559b935a8c26fe04f1a7a36c7ab
laurene michon <laurene.michon@gmail.com>
2020-11-17 13:10:17 +0100

    MVC done, cloudinary and active storage

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -8,6 +8,7 @@ class Animal < ApplicationRecord
   validates :power, inclusion: { in: POWERS }
   validates :name, presence: true, uniqueness: true
   validates :price, :category, :location, presence: true
+  has_many_attached :photos
 end
 
 # user1 = User.create!(email: "toto@set.fr", password: "abcdef", password_confirmation: "abcdef")
